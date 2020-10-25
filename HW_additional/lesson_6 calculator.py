5bf0e17e24d0fd797059300ab1291b13283945de
anvan <ans.vanyan@gmail.com>
2020-10-25 13:43:04 +0300

    upgrade calculator output and docstings

--- HW_additional/lesson_6 calculator.py
+++ HW_additional/lesson_6 calculator.py
@@ -2,20 +2,19 @@
 пока что калькулятор может:
 1. принимать введенное выражение и очищать его от пробелов
 2. определять многозначные числа и знак **  и преобразование в итоговый список для вычислений
-3. выполняет операций +- */ ** с 2 числами
-4. калькулятор для выражений с множеством чисел - с операциями одного уровня - не понимает приоритет (без скобок)
+3. калькулятор для выражений с множеством чисел - с операциями одного уровня - не понимает приоритет (без скобок)
    4.1. 1 уровень - **; 2 уровень - */; 3 уровень - +-.
    Пример: 3 + 10 - 2 или 3 * 10 / 2
 
 далее:
-5. калькулятор с операциями разного уровня - понимает приоритет (без скобок)
-6. калькулятор со скобками + с пониманием отрицательных числе в виде (-3)
-7. почистить код + написать с применением ООП
+4. калькулятор с операциями разного уровня - понимает приоритет (без скобок)
+5. калькулятор со скобками + с пониманием отрицательных числе в виде (-3)
+6. почистить код + написать с применением ООП
 """
 
 
-# func - calculator for two numbers (operations: + - / * **)
 def math_operation(expression):
+    """ calculator for two first numbers in expression (operations: + - / * **) """
     operator = expression[1]
     if operator == '**':
         return expression[0]**expression[2]
@@ -29,12 +28,15 @@ def math_operation(expression):
         return expression[0]-expression[2]
 
 
-# func - calculator for expressions with multiple numbers - with operations of the same level
-# - does not understand priority
 def calc_one_level(expression):
+    """
+    calculator for expressions with several numbers - with operations of the same level
+    does not understand priority
+    Example: 3 + 10 - 2 or 3 * 10 / 2
+    """
     while len(expression) > 1:
-        operation = math_operation(expression)
-        expression.insert(0, operation)
+        result = math_operation(expression)
+        expression.insert(0, result)
         del expression[2+1]
         del expression[1+1]
         del expression[0+1]
@@ -83,4 +85,4 @@ print(exp)
 # calculator for expressions with multiple numbers - with operations of the same level
 # - does not understand priority
 result = calc_one_level(exp)
-print(f'result: {result}')
+print(f'result: {result[0]}')
