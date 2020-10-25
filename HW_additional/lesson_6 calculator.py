2ec616ebdf32f29487d71fcd6b1fbd2a83d6a4a5
anvan <ans.vanyan@gmail.com>
2020-10-25 09:40:00 +0300

    upgrade calculator

--- HW_additional/lesson_6 calculator.py
+++ HW_additional/lesson_6 calculator.py
@@ -1,24 +1,33 @@
-# string = input(f'Input: ')
+""" пока что калькулятор может:
+1. принимать введенное выражение,
+2. очищать выражение от пробелов
+3. определять многозначные числа в выражении и переводить их в int
+4.
 
-s = '23 + 3'
-s_list = list(s)
-print(s_list)
-
-for i in s_list:
-    if i == ' ':
-        s_list.remove(i)
-print(s_list)
-
-if s_list[0].isdigit():
-    s_list[0]
-    print('ok')
+применить алгоритм пинг-понг"""
 
 
-# s_list_new = []
-#
-# for el in s_list:
-#     if el.isdigit():
-#         s_list_new.append()
-#     else:
-#         pass
+# input expression and convert to the list
+exp_input = list(input(f'Input math expression: '))
+# print(exp_input)
 
+
+# clear the list of spaces
+exp_clear = list(filter(lambda x: x != ' ', exp_input))
+# print(exp_clear)
+
+
+# find multi-digit numbers and create new list with int
+exp = []
+i = 0
+while i < len(exp_clear):
+    number = ''
+    while i < len(exp_clear) and exp_clear[i].isdigit():
+        number += exp_clear[i]
+        if i+1 == len(exp_clear) or not exp_clear[i+1].isdigit():
+            exp.append(int(number))
+        i += 1
+    if i < len(exp_clear):
+        exp.append(exp_clear[i])
+    i += 1
+print(exp)
